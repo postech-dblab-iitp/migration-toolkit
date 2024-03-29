632866b976a708d588aa1756712368eae851fd63
Kang-dot <jgbu97@gmail.com>
2024-03-29 16:04:04 +0900

    [GT-103] tibero에서 hint 사용하도록 쿼리 개선
    - tibero에서 edge를 csv로 추출할 때 use_merge hint 사용하도록 개선

--- com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/tibero/export/TiberoExportHelper.java
+++ com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/tibero/export/TiberoExportHelper.java
@@ -275,7 +275,7 @@ public class TiberoExportHelper extends
 		
 		String innerTableWhere = null;
 		
-		buffer.append("SELECT * FROM (SELECT rownum as rnum, ");
+		buffer.append("SELECT * /*+ use_merge */FROM (SELECT rownum as rnum, ");
 		buffer.append(startIdWithVertexName);		
 		buffer.append(", ");
 		buffer.append(endIdWithVertexName);
@@ -346,7 +346,7 @@ public class TiberoExportHelper extends
 	@Override
 	public String getJoinTableInnerQuery(Edge e, String sql, Connection conn, long innerTotalExported, long realPageCount) {
 		
-		sql = sql.replaceFirst("SELECT", "SELECT ROWNUM AS rnum, ");
+		sql = sql.replaceFirst("SELECT", "SELECT /*+ use_merge */ ROWNUM AS rnum, ");
 		
 		String buffer = new String(sql);
 		

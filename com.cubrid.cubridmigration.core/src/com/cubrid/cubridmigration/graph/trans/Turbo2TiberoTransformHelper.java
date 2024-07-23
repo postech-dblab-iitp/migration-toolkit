d74be67bd4b4ebcdf8ec5b56120c659e004edc3f
Kang-dot <jgbu97@gmail.com>
2024-07-23 12:48:21 +0900

    [GT-156] turbo to tibero type mapping helper 구현

---
+++ com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/graph/trans/Turbo2TiberoTransformHelper.java
@@ -0,0 +1,30 @@
+package com.cubrid.cubridmigration.graph.trans;
+
+import com.cubrid.cubridmigration.core.dbobject.Column;
+import com.cubrid.cubridmigration.core.engine.config.MigrationConfiguration;
+import com.cubrid.cubridmigration.core.mapping.AbstractDataTypeMappingHelper;
+import com.cubrid.cubridmigration.core.trans.DBTransformHelper;
+import com.cubrid.cubridmigration.cubrid.trans.ToCUBRIDDataConverterFacade;
+
+public class Turbo2TiberoTransformHelper extends DBTransformHelper {
+
+	public Turbo2TiberoTransformHelper(
+			AbstractDataTypeMappingHelper dataTypeMapping,
+			ToTiberoDataConverterFacade cf) {
+		super(dataTypeMapping, cf);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void adjustPrecision(Column srcColumn, Column cubridColumn,
+			MigrationConfiguration config) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getGraphDataType(Column col, MigrationConfiguration cfg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}

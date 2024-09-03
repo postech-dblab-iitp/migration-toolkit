b8386d67b6ee282b9b586d5edd38cb79d0e60036
KangBu Jeong <54855268+Kang-dot@users.noreply.github.com>
2024-09-03 10:38:11 +0900

    [GT-135] 오브젝트 이름 변경에 대한 redo, undo 기능 추가 (#143)

--- com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/graph/dbobj/Work.java
+++ com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/graph/dbobj/Work.java
@@ -1,19 +1,37 @@
 package com.cubrid.cubridmigration.graph.dbobj;
 
+import com.cubrid.cubridmigration.core.dbobject.DBObject;
+
 public class Work {
 	private int workType;
-	private Edge edge;
+	private DBObject object;
+	private String originalName;
 	
-	public Work(Edge edge, int workType) {
-		this.edge = edge;
+	public Work(int workType, DBObject obj) {
 		this.workType = workType;
+		this.object = obj;
 	}
 	
-	public Edge getEdge() {
-		return this.edge;
+	public Work(int workType, DBObject object, String originalName) {
+		this.workType = workType;
+		this.object = object;
+		this.originalName = originalName;
 	}
-	
+
 	public int getWorkType() {
 		return this.workType;
 	}
+	
+	public DBObject getObject() {
+		return this.object;
+	}
+	
+	public String getOriginalName() {
+		return this.originalName;
+	}
+
+	public void setOriginalName(String name) {
+		this.originalName = name;
+	}
+	
 }
